b04bf4153c76089d9e0ec0db6b99995c9df379de
jh1tak <jh_tak@naver.com>
2025-04-29 12:18:11 +0000

    update

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
-def verify():
-    if request.is_json:
-        data = request.get_json()
-        if data.get('type') == 'url_verification':
-            return jsonify({'challenge': data['challenge']})
-    return '', 404
+def slack_event():
+    data = request.get_json()
+    # Slack의 URL 검증용 challenge 응답
+    if 'challenge' in data:
+        return jsonify({'challenge': data['challenge']})
+
+    # 이후 Slack 메시지 이벤트도 여기서 처리할 수 있음
+    return '', 200
 
 @app.route('/', methods=['GET'])
-def health_check():
+def health():
     return 'OK', 200
 
 if __name__ == '__main__':
